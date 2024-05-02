e074ed48d9b33d092a0b4381c21c13f5e1bce3fa
Yallamaztar <budi.world@yahoo.com>
2024-05-02 22:31:37 +0300

    TonyBot V1.0

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import os, json
 from datetime import datetime
 import aiohttp
 import uuid
-import asyncio, threading
+import asyncio
 from dotenv import load_dotenv
 
 from src.utils import (
@@ -16,16 +16,22 @@ from src.utils import (
     writeJson,
     createEmbed,
     menu,
-    initRoles
+    initRoles,
+    initNottKick
 )
 
+# Loading env vars
 load_dotenv()
-
+TOKEN   = os.getenv('TOKEN')
 BASEURI = os.getenv('BASEURI')
 SND_ID  = os.getenv('SND_ID')
 FFA_ID  = os.getenv('FFA_ID')
 COOKIE  = os.getenv('HEADERS')
 
+# Loading Config, could be done better
+with open('src/config.json') as f:
+    config = json.load(f)
+GUILD_ID: int = config['guild_id']
 logs = []
 
 # change to your server role ids
@@ -33,41 +39,45 @@ OWNER_ID = 1190796076379283536
 SENIOR_ID = 1190794949353029664
 ADMIN_ID = 1190703210973904957
 
-bot = commands.Bot(command_prefix="!", Intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
+# On bot ready
 @bot.event
 async def on_ready():
     await clear()
     await menu(bot)
     await bot.change_presence(activity=discord.Game(name="with myself"), status=discord.Status.idle)
     
+# On bot join
 @bot.event
 async def on_guild_join(guild):
-    global GUILD_ID
-    global CHANNEL_ID
     guildId = guild.id
     guildName = guild.name.capitalize()
+
     config_data = await loadJson('src/config.json')
     config_data['guild_id'] = guildId
+
     roles = await initRoles(guild)
     config_data['roles'] = roles
+
     await writeJson(config_data, 'src/config.json')
-    with open('src/config.json') as f:
-        GUILD_ID: int = config['guild_id']
-        CHANNEL_ID = config['channel_id']
+
     print(f"\n[ \x1B[38;2;245;0;245mJoined\x1b[0m ]")
     print(f"[ \x1B[38;2;245;0;245mServer:\x1b[0m {guildName} ]")
     print(f"[ \x1B[38;2;245;0;245mGuild:\x1b[0m {guildId} ]")
 
+# On bot command
 @bot.event
 async def on_application_command(ctx: discord.ApplicationContext):
     await clear()
     await menu(bot)
     
+    # Log schema
     id = str(uuid.uuid4())
-    timestamp = datetime.now().strftime("%Y-%m-%d - %H:%M:%S")
     author = ctx.author.mention
     command_name = ctx.command.name
+
+    timestamp = datetime.now().strftime("%Y-%m-%d - %H:%M:%S")
     
     print("\n[ \x1B[38;2;245;0;245mCommand Received\x1b[0m ]")
     print(f"[ \x1B[38;2;245;0;245mID:\x1b[0m {id} ]")
@@ -85,12 +95,14 @@ async def on_application_command(ctx: discord.ApplicationContext):
     logs.append(data)
     await writeJson(logs, 'src/logs.json')
     
-    await asyncio.sleep(1)
+    await asyncio.sleep(5)
     await clear()
     await menu(bot)
 
 @bot.event 
 async def on_message(message):
+    if message.author == bot.user:
+        return
     if message.channel.id != 1233410655055511654:
         return
     else:
@@ -105,7 +117,6 @@ async def on_application_command_error(ctx: discord.ApplicationContext,
             ephemeral=True,
         )
     elif (type(error) == commands.CommandOnCooldown):
-        # TODO use clearer regex
         await ctx.send_response(
             f'This command is on cooldown! Try again in {error.args[0].split(" ")[-1].replace("s", "")} seconds!',
             ephemeral=True,
@@ -114,12 +125,10 @@ async def on_application_command_error(ctx: discord.ApplicationContext,
         raise error
 
 @bot.slash_command(guild_ids=[GUILD_ID], name="setup", description="Setup tony bot")
+@commands.has_any_role(OWNER_ID)
 async def setupTony(ctx: discord.ApplicationContext):  
     options = []
     for guild in bot.guilds:
-        if ctx.author.id != 996056057493323808:
-            await ctx.respond("You dont have permission to run this command", ephemeral=True)
-            return
         for channel in guild.text_channels:
             options.append(discord.SelectOption(label=channel.name, value=str(channel.id)))
     
@@ -142,24 +151,27 @@ async def setupTony(ctx: discord.ApplicationContext):
         await channel.send(embed=await createEmbed(discord, 'src/embeds/console.json'))
         await interaction.response.defer()
         
-        await message.delete()
-        
     select.callback = successSetup
     view = View()
     view.add_item(select)
     await ctx.respond(embed=await createEmbed(discord, 'src/embeds/setup.json'), view=view, ephemeral=True)
-    
-# ban a player
+
+@commands.cooldown(1, 3, commands.BucketType.user)
 @bot.slash_command(guild_ids=[GUILD_ID], name="ban", description="Ban a player")
 @bot.slash_command(guild_ids=[GUILD_ID], name="b", description="Ban a player")
 @commands.has_any_role(OWNER_ID, SENIOR_ID)
-@option("player", description="Name or Xuid (if using xuid, please enter like '@<xui>')")
+@option("server", description="ffa or snd", choices=["ffa", "snd"])
+@option("player", description="Name or Xuid (if using xuid, please enter like '@<xuid>')")
 @option("reason", description="Reason")
-async def ban(ctx, player, reason):
+async def ban(ctx, server, player, reason):
     header = {'Cookie': COOKIE}
     async with aiohttp.ClientSession(headers=header) as session:
         url = "http://141.11.196.83:1624/Console/Execute"
-        params = {"serverId": "14111196834977", "command": f"!b {player} {reason}"}
+        server.lower()
+        if server == "ffa":
+            params = {"serverId": "14111196834979", "command": f"!b {player} {reason}"}
+        elif server == "snd":
+            params = {"serverId": "14111196834977", "command": f"!b {player} {reason}"}
         async with session.get(url, params=params) as req:
             if req.status == 200:
                 await ctx.respond(f"Successfully banned player with Id: {player} for reason: {reason}", ephemeral=True)
@@ -167,16 +179,22 @@ async def ban(ctx, player, reason):
                 await ctx.respond(f"Server Down Or Player Not Found: {req.status}", ephemeral=True)
                 return 
 
+@commands.cooldown(1, 3, commands.BucketType.user)
 @bot.slash_command(guild_ids=[GUILD_ID], name="unban", description="Unban a player")
 @bot.slash_command(guild_ids=[GUILD_ID], name="ub", description="Unban a player")
 @commands.has_any_role(OWNER_ID, SENIOR_ID)
-@option("player", description="Name or Xuid (if using xuid, please enter like '@<xui>')")
+@option("server", description="ffa or snd", choices=["ffa", "snd"])
+@option("player", description="Name or Xuid (if using xuid, please enter like '@<xuid>')")
 @option("reason", description="Reason")
-async def unban(ctx, player, reason):
+async def unban(ctx, server, player, reason):
     header = {'Cookie': COOKIE}
     async with aiohttp.ClientSession(headers=header) as session:
         url = "http://141.11.196.83:1624/Console/Execute"
-        params = {"serverId": "14111196834977", "command": f"!ub {player} {reason}"}
+        server.lower()
+        if server == "ffa":
+            params = {"serverId": "14111196834979", "command": f"!ub {player} {reason}"}
+        elif server == "snd":
+            params = {"serverId": "14111196834977", "command": f"!ub {player} {reason}"}
         async with session.get(url, params=params) as req:
             if req.status == 200:
                 await ctx.respond(f"Successfully unbanned player: {player} for reason: {reason}", ephemeral=True)  
@@ -184,17 +202,23 @@ async def unban(ctx, player, reason):
                 await ctx.respond(f"Server Down Or Player Not Found: {req.status}", ephemeral=True)
                 return 
 
+@commands.cooldown(1, 3, commands.BucketType.user)
 @bot.slash_command(guild_ids=[GUILD_ID], name="tempban", description="Tempban a player")
 @bot.slash_command(guild_ids=[GUILD_ID], name="tb", description="Tempban a player")
 @commands.has_any_role(OWNER_ID, SENIOR_ID, ADMIN_ID)
-@option("player", description="Name or Xuid (if using xuid, please enter like '@<xui>')")
+@option("server", description="ffa or snd", choices=["ffa", "snd"])
+@option("player", description="Name or Xuid (if using xuid, please enter like '@<xuid>')")
 @option("duration", description="<duration (m|h|d|w|y)>")
 @option("reason", description="Reason")
-async def tempban(ctx, player, duration, reason):
+async def tempban(ctx, server, player, duration, reason):
     header = {'Cookie': COOKIE}
     async with aiohttp.ClientSession(headers=header) as session:
         url = "http://141.11.196.83:1624/Console/Execute"
-        params = {"serverId": "14111196834977", "command": f"!tb {player} {duration} {reason}"}
+        server.lower()
+        if server == "ffa":
+            params = {"serverId": "14111196834979", "command": f"!tb {player} {duration} {reason}"}
+        elif server == "snd":
+            params = {"serverId": "14111196834977", "command": f"!tb {player} {duration} {reason}"}
         async with session.get(url, params=params) as req:
             if req.status == 200:
                 await ctx.respond(f"Successfully tempbanned player: {player} for reason: {reason}", ephemeral=True)  
@@ -202,16 +226,22 @@ async def tempban(ctx, player, duration, reason):
                 await ctx.respond(f"Server Down Or Player Not Found: {req.status}", ephemeral=True)
                 return 
 
+@commands.cooldown(1, 3, commands.BucketType.user)
 @bot.slash_command(guild_ids=[GUILD_ID], name="flag", description="Flag a player")
 @bot.slash_command(guild_ids=[GUILD_ID], name="f", description="Flag a player")
 @commands.has_any_role(OWNER_ID, SENIOR_ID, ADMIN_ID)
-@option("player", description="Name or Xuid (if using xuid, please enter like '@<xui>')")
+@option("server", description="ffa or snd", choices=["ffa", "snd"])
+@option("player", description="Name or Xuid (if using xuid, please enter like '@<xuid>')")
 @option("reason", description="Reason")
-async def flag(ctx, player, reason):
+async def flag(ctx, server, player, reason):
     header = {'Cookie': COOKIE}
     async with aiohttp.ClientSession(headers=header) as session:
         url = "http://141.11.196.83:1624/Console/Execute"
-        params = {"serverId": "14111196834977", "command": f"!f {player} {reason}"}
+        server.lower()
+        if server == "ffa":
+            params = {"serverId": "14111196834979", "command": f"!flag {player} {reason}"}
+        elif server == "snd":
+            params = {"serverId": "14111196834977", "command": f"!flag {player} {reason}"}
         async with session.get(url, params=params) as req:
             if req.status == 200:
                 await ctx.respond(f"Successfully flagged player: {player} for reason: {reason}", ephemeral=True)  
@@ -219,16 +249,22 @@ async def flag(ctx, player, reason):
                 await ctx.respond(f"Server Down Or Player Not Found: {req.status}", ephemeral=True)
                 return 
 
+@commands.cooldown(1, 3, commands.BucketType.user)
 @bot.slash_command(guild_ids=[GUILD_ID], name="unflag", description="Unflag a player")
 @bot.slash_command(guild_ids=[GUILD_ID], name="uf", description="Unflag a player")
 @commands.has_any_role(OWNER_ID, SENIOR_ID, ADMIN_ID)
-@option("player", description="Name or Xuid (if using xuid, please enter like '@<xui>')")
+@option("server", description="ffa or snd", choices=["ffa", "snd"])
+@option("player", description="Name or Xuid (if using xuid, please enter like '@<xuid>')")
 @option("reason", description="Reason")
-async def unflag(ctx, player, reason):
+async def unflag(ctx, server, player, reason):
     header = {'Cookie': COOKIE}
     async with aiohttp.ClientSession(headers=header) as session:
         url = "http://141.11.196.83:1624/Console/Execute"
-        params = {"serverId": "14111196834977", "command": f"!uf {player} {reason}"}
+        server.lower()
+        if server == "ffa":
+            params = {"serverId": "14111196834979", "command": f"!uf {player} {reason}"}
+        elif server == "snd":
+            params = {"serverId": "14111196834977", "command": f"!uf {player} {reason}"}
         async with session.get(url, params=params) as req:
             if req.status == 200:
                 await ctx.respond(f"Successfully unflagged player: {player} for reason: {reason}", ephemeral=True)  
@@ -236,16 +272,22 @@ async def unflag(ctx, player, reason):
                 await ctx.respond(f"Server Down Or Player Not Found: {req.status}", ephemeral=True)
                 return  
 
+@commands.cooldown(1, 3, commands.BucketType.user)
 @bot.slash_command(guild_ids=[GUILD_ID], name="warn", description="Warn a player")
 @bot.slash_command(guild_ids=[GUILD_ID], name="w", description="Warn a player")
 @commands.has_any_role(OWNER_ID, SENIOR_ID, ADMIN_ID)
-@option("player", description="Name or Xuid (if using xuid, please enter like '@<xui>')")
+@option("server", description="ffa or snd", choices=["ffa", "snd"])
+@option("player", description="Name or Xuid (if using xuid, please enter like '@<xuid>')")
 @option("reason", description="Reason")
-async def warn(ctx, player, reason):
+async def warn(ctx, server, player, reason):
     header = {'Cookie': COOKIE}
     async with aiohttp.ClientSession(headers=header) as session:
         url = "http://141.11.196.83:1624/Console/Execute"
-        params = {"serverId": "14111196834977", "command": f"!w {player} {reason}"}
+        server.lower()
+        if server == "ffa":
+            params = {"serverId": "14111196834979", "command": f"!w {player} {reason}"}
+        elif server == "snd":
+            params = {"serverId": "14111196834977", "command": f"!w {player} {reason}"}
         async with session.get(url, params=params) as req:
             if req.status == 200:
                 await ctx.respond(f"Successfully Warned player: {player} for reason: {reason}", ephemeral=True)  
@@ -253,15 +295,21 @@ async def warn(ctx, player, reason):
                 await ctx.respond(f"Server Down Or Player Not Found: {req.status}", ephemeral=True)
                 return 
 
+@commands.cooldown(1, 3, commands.BucketType.user)
 @bot.slash_command(guild_ids=[GUILD_ID], name="warnclear", description="Clear warnings from a player")
 @bot.slash_command(guild_ids=[GUILD_ID], name="wc", description="Clear warnings from a player")
 @commands.has_any_role(OWNER_ID, SENIOR_ID, ADMIN_ID)
-@option("player", description="Name or Xuid (if using xuid, please enter like '@<xui>')")
-async def warnclear(ctx, player):
+@option("server", description="ffa or snd", choices=["ffa", "snd"])
+@option("player", description="Name or Xuid (if using xuid, please enter like '@<xuid>')")
+async def warnclear(ctx, server, player):
     header = {'Cookie': COOKIE}
     async with aiohttp.ClientSession(headers=header) as session:
         url = "http://141.11.196.83:1624/Console/Execute"
-        params = {"serverId": "14111196834977", "command": f"!wc {player}"}
+        server.lower()
+        if server == "ffa":
+            params = {"serverId": "14111196834979", "command": f"!wc {player}"}
+        elif server == "snd":
+            params = {"serverId": "14111196834977", "command": f"!wc {player}"}
         async with session.get(url, params=params) as req:
             if req.status == 200:
                 await ctx.respond(f"Successfully Cleared warnings from player: {player}", ephemeral=True)  
@@ -269,16 +317,22 @@ async def warnclear(ctx, player):
                 await ctx.respond(f"Server Down Or Player Not Found: {req.status}", ephemeral=True)
                 return 
 
+@commands.cooldown(1, 3, commands.BucketType.user)
 @bot.slash_command(guild_ids=[GUILD_ID], name="kick", description="Kick a player")
 @bot.slash_command(guild_ids=[GUILD_ID], name="k", description="Kick a player")
 @commands.has_any_role(OWNER_ID, SENIOR_ID, ADMIN_ID)
-@option("player", description="Name or Xuid (if using xuid, please enter like '@<xui>')")
+@option("server", description="ffa or snd", choices=["ffa", "snd"])
+@option("player", description="Name or Xuid (if using xuid, please enter like '@<xuid>')")
 @option("reason", description="Reason")
-async def kick(ctx, player, reason):
+async def kick(ctx, server, player, reason):
     header = {'Cookie': COOKIE}
     async with aiohttp.ClientSession(headers=header) as session:
         url = "http://141.11.196.83:1624/Console/Execute"
-        params = {"serverId": "14111196834977", "command": f"!k {player} {reason}"}
+        server.lower()
+        if server == "ffa":
+            params = {"serverId": "14111196834979", "command": f"!kick {player} {reason}"}
+        elif server == "snd":
+            params = {"serverId": "14111196834977", "command": f"!kick {player} {reason}"}
         async with session.get(url, params=params) as req:
             if req.status == 200:
                 await ctx.respond(f"Successfully Warned player: {player} for reason: {reason}", ephemeral=True)  
@@ -286,20 +340,45 @@ async def kick(ctx, player, reason):
                 await ctx.respond(f"Server Down Or Player Not Found: {req.status}", ephemeral=True)
                 return 
 
+@commands.cooldown(1, 3, commands.BucketType.user)
 @bot.slash_command(guild_ids=[GUILD_ID], name="say", description="Broadcast message to all clients")
 @bot.slash_command(guild_ids=[GUILD_ID], name="s", description="Broadcast message to all clients")
 @commands.has_any_role(OWNER_ID, SENIOR_ID, ADMIN_ID)
+@option("server", description="ffa or snd", choices=["ffa", "snd"])
 @option("message", description="Your message")
-async def say(ctx, message):
+async def say(ctx, server, message):
     header = {'Cookie': COOKIE}
     async with aiohttp.ClientSession(headers=header) as session:
         url = "http://141.11.196.83:1624/Console/Execute"
-        params = {"serverId": "14111196834977", "command": f"!s {message}"}
+        server.lower()
+        if server == "ffa":
+            params = {"serverId": "14111196834979", "command": f"!say {message}"}
+        elif server == "snd":
+            params = {"serverId": "14111196834977", "command": f"!say {message}"}
         async with session.get(url, params=params) as req:
             if req.status == 200:
                 await ctx.respond(f"Successfull", ephemeral=True)
             else:
                 await ctx.respond(f"Server Down Or Player Not Found: {req.status}", ephemeral=True)
                 return    
+            
 
-bot.run(os.getenv('TOKEN'))
+@commands.cooldown(1, 3, commands.BucketType.user)
+@bot.slash_command(guild_ids=[GUILD_ID], name="tony", description="Kick nott GG")
+@commands.has_any_role(OWNER_ID, SENIOR_ID)
+@option("message", description="Message for nott")
+async def tony(ctx, message):
+    await initNottKick(message)
+    await ctx.respond("lol", ephemeral=True)
+    return
+
+
+@bot.command()
+async def nottUpdate(ctx):
+    await ctx.message.delete()
+    await asyncio.sleep(2)
+    await ctx.respond("Senior Admins Now Can Now Kick Notthepro with: '/tony <reason>'", ephemeral=True)
+    return
+
+
+bot.run(TOKEN)
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,10 @@
 import os, platform
 import json
-import asyncio, time
+import asyncio, aiohttp
+from yt_dlp import YoutubeDL
+
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
 async def clear():
     if platform.system() == "Windows":
@@ -33,7 +37,6 @@ async def createEmbed(discord, file_path):
         embed.set_author(name=embed_config['username'], icon_url=embed_config.get('avatar_url', None))
     return embed
 
-
 async def menu(bot, alert=None):
     print('''\n
 \x1B[38;2;245;0;245m ████████╗ ██████╗ ███╗   ██╗██╗   ██╗    ██████╗  ██████╗ ████████╗    
@@ -43,8 +46,48 @@ async def menu(bot, alert=None):
 \x1B[38;2;165;0;245m    ██║   ╚██████╔╝██║ ╚████║   ██║       ██████╔╝╚██████╔╝   ██║       
 \x1B[38;2;145;0;245m    ╚═╝    ╚═════╝ ╚═╝  ╚═══╝   ╚═╝       ╚═════╝  ╚═════╝    ╚═╝ \x1b[0m      
     ''')
-    print(f'[ Admin Panel ] [ v1.1 ] {f"[ {alert} ]" if alert else ""}\n')
     print(f'[ \x1B[38;2;245;0;245mConnected\x1b[0m ]\n[ \x1B[38;2;245;0;245mBot:\x1b[0m {bot.user.name} ]\n[ \x1B[38;2;245;0;245mID:\x1b[0m {bot.user.id} ]')
-    print("[1] Broadcast a message\n")
+
+
+BROWNIES_BASEURI = "http://141.11.196.83:1624"
+
+# SnD
+BROWNIES_SND = f"{BROWNIES_BASEURI}/Console/Execute?serverId=14111196834977&"
+BROWNIES_SND_STATUS = "http://141.11.196.83:1624/api/Status/?id=14111196834977"
+
+# FFA
+BROWNIES_FFA = f"{BROWNIES_BASEURI}/Console/Execute?serverId=14111196834979&"
+BROWNIES_FFA_STATUS = "http://141.11.196.83:1624/api/Status/?id=14111196834979"
+
+nott_users = ["Notthepro", "xxmlggyatxx", "mlgproconfirmed", "yesthebot"]
+
+async def browniesStatus(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as res:
+            data = await res.json()
+            players = [player['name'] for player in data[0]['players']]
+            return players
+        
+async def initNottKick(reason):
+    snd_name = await browniesStatus(BROWNIES_SND_STATUS)
+    for name in snd_name:
+        if name in nott_users:
+            await antiNottScript(BROWNIES_SND, name, reason)
+    ffa_name = await browniesStatus(BROWNIES_FFA_STATUS)
+    for name in ffa_name:
+        if name in nott_users:
+            await antiNottScript(BROWNIES_FFA, name, reason)
+
+async def antiNottScript(url, name, reason):
+    header = {'Cookie': os.getenv('HEADERS')}
+    params = { "command": f"!w {name} ^0^F{reason}" }
+    async with aiohttp.ClientSession(headers=header) as session:
+        for _ in range(4):
+            async with session.get(url, params=params) as req:
+                print(req)
+            await asyncio.sleep(0.5)
+
+
+
 
 
